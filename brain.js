0d78950f18660e909fe76e327b5d6c8607c9cd43
LordChrys <49178040+LordChrys@users.noreply.github.com>
2022-10-16 16:54:43 +1300

    000 - Grocery list
    Updated from todo list to grocery list.
    Starting JS from scratch with no tutorial assistance

---
+++ brain.js
@@ -0,0 +1,25 @@
+const input = document.getElementsByClassName('user-input');
+const add = document.getElementsByClassName('add-new');
+const list = document.getElementsByClassName('list-items');
+const item = document.getElementsByClassName('item')
+
+
+const newItem = (item)=>{
+    const temp = `
+    <li class="item" style="list-style-type: none;">
+    <span>${item}</span>
+    <button class="delete"> x </button>
+    </li>`
+
+    list.innerHTML += temp;
+}
+
+
+/*
+DOM the add button
+DOM user input
+DOM the whole list
+DOM list item for template
+
+Function to add user input to list 
+*/ 
